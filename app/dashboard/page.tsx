cb8c62d6b25cf409ab7f37547ec11442dce3ed2a
kaleem <mokaleem@users.noreply.github.com>
2024-12-05 00:01:25 -0600

    added phone mockup

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,19 @@
+// import { auth } from "@clerk/nextjs/server";
 import React from "react";
+import ChatInterface from "./_components/ChatInterface";
+import Phone from "./_components/Phone";
 
 function DashboardPage() {
-  return <div>DashboardPage</div>;
+  // const { userId, redirectToSignIn } = auth();
+  // if (!userId) return redirectToSignIn();
+
+  return (
+    <section className="container">
+      <div>
+        <Phone />
+      </div>
+    </section>
+  );
 }
 
 export default DashboardPage;
