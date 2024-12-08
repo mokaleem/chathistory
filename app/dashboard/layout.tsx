92a84e5c9b689dab4ab69b366b35f036f9dadf29
kaleem <mokaleem@users.noreply.github.com>
2024-12-08 14:19:34 -0600

    added sidebar to dashboard page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,18 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import Navbar from "./_components/Navbar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-accent/5 min-h-screen">
-      <Navbar />
-      <div className="container mx-auto px-4 py-8">{children}</div>
-    </div>
+    <SidebarProvider>
+      <div className="flex h-screen overflow-hidden">
+        <AppSidebar />
+        <div>
+          <SidebarTrigger />
+          {children}
+        </div>
+      </div>
+    </SidebarProvider>
   );
 }
 
