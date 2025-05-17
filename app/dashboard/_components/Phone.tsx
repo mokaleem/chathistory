93fd1e768bb5ee6fe9b7cd34da99a4e3e3c0b4fe
kaleem <mokaleem@users.noreply.github.com>
2025-05-17 14:27:22 -0500

    updated phone area

--- app/dashboard/_components/Phone.tsx
+++ app/dashboard/_components/Phone.tsx
@@ -6,13 +6,13 @@ import { PhoneMessagesClient } from "./PhoneMessagesClient";
 
 function Phone() {
   return (
-    <div className="relative mx-auto border-gray-800 bg-gray-800 border-[6px] rounded-[2.5rem] h-[601px] w-[292px] phone-area-component max-h-screen">
+    <div className="relative mx-auto border-gray-800 bg-gray-800 border-[10px] rounded-[2.5rem] h-[607px] w-[320px] phone-area-component">
       <div className="w-[90px] h-[28px] bg-gray-800 rounded-[1rem] left-1/2 -translate-x-1/2 absolute top-1"></div>
-      <div className="h-[32px] w-[4px] bg-gray-800 dark:bg-gray-800 absolute -start-[8px] top-[72px] rounded-s-lg"></div>
-      <div className="h-[46px] w-[4px] bg-gray-800 dark:bg-gray-800 absolute -start-[8px] top-[124px] rounded-s-lg"></div>
-      <div className="h-[46px] w-[4px] bg-gray-800 dark:bg-gray-800 absolute -start-[8px] top-[178px] rounded-s-lg"></div>
-      <div className="h-[64px] w-[4px] bg-gray-800 dark:bg-gray-800 absolute -end-[8px] top-[142px] rounded-e-lg"></div>
-      <div className="rounded-[2rem] overflow-hidden w-[280px] h-[587px] !bg-white z-10">
+      <div className="h-[32px] w-[4px] bg-gray-800 dark:bg-gray-800 absolute -start-[12px] top-[72px] rounded-s-lg"></div>
+      <div className="h-[46px] w-[4px] bg-gray-800 dark:bg-gray-800 absolute -start-[12px] top-[124px] rounded-s-lg"></div>
+      <div className="h-[46px] w-[4px] bg-gray-800 dark:bg-gray-800 absolute -start-[12px] top-[178px] rounded-s-lg"></div>
+      <div className="h-[64px] w-[4px] bg-gray-800 dark:bg-gray-800 absolute -end-[12px] top-[142px] rounded-e-lg"></div>
+      <div className="rounded-[2rem] overflow-hidden w-[302px] h-[587px] !bg-white z-10">
         <div style={{ background: "e3dcd3" }}>
           <NetworkBar />
           <NameBar />
